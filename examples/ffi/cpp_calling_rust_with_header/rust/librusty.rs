e3f1361727bd34e95783da0a326297f3154a5244
Vinh Tran <vinhdaitran@google.com>
2023-09-28 18:13:24 +0000

    Fix clippy error

--- examples/ffi/cpp_calling_rust_with_header/rust/librusty.rs
+++ examples/ffi/cpp_calling_rust_with_header/rust/librusty.rs
@@ -1,6 +1,6 @@
 //! A simple hello world example that can be called from C
 
 #[no_mangle]
-pub unsafe extern "C" fn print_c_hello_rust() {
+pub fn print_c_hello_rust() {
     println!("Hello Rust!");
 }
